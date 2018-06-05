8f1ab2d2b70ad5b306b09d090eeb921a6bafe940
github username <github email address>
2018-06-05 14:56:07 -0400

    WORKING STAR WARS

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,27 +6,43 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  collected_films = []
+  films_hash = {}
+  character_hash["results"].each do |hash|
+    hash.find do |name, value|
+      if name == "name" && value.downcase == "#{character}"
+        hash["films"].each do |url|
+          collected_films << url
+        end
+      end
+    end
+  end
+  collected_films.each do |url|
+    film_api = RestClient.get(url)
+    film_info_hash = JSON.parse(film_api)
+    films_hash[url] = film_info_hash
+  end
+  films_hash
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  titles_array = []
+  films_hash.each do |url, movie_data|
+    movie_data.find do |key, title|
+      if key == "title"
+        titles_array << title
+      end
+    end
+  end
+  titles_array.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+require "pry"
+
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to a galaxy. Far, far away, it is."
 end
 
 def get_character_from_user
   puts "please enter a character"
+  input = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
