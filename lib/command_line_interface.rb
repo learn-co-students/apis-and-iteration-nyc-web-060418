f0c8a726ae99d45b6adfb9877becc4638a4092ca
Jee Yoon Lee <jlee66@wellesley.edu>
2018-06-05 20:09:03 +0000

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,6 +1,10 @@
 def welcome
   # puts out a welcome message here!
+<<<<<<< HEAD
   puts "welcome"
+=======
+  puts welcome
+>>>>>>> c7de6edb233641fbf81dacc41764b9960b1ed39c
 end
 
 def get_character_from_user
