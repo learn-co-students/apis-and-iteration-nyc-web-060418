b4e57111fcb438cae562e3125179b4f23135ebc0
noah-berman <noahberm@gmail.com>
2018-06-05 15:14:48 -0400

    completed base assignment

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,7 +4,12 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+
+  #counter = 1
+  #puts "Going through again time number #{counter}"
+  api_link = 'http://www.swapi.co/api/people/' #?page= #+ counter.to_s
+
+  all_characters = RestClient.get(api_link)
   character_hash = JSON.parse(all_characters)
 
 
@@ -12,7 +17,11 @@ def get_character_movies_from_api(character)
   #   `character`
   film_array = []
   character_hash["results"].each do |attributes|
-    film_array = attributes["films"]
+    if attributes["name"] == character
+      film_array = attributes["films"]
+    #else
+      #counter += 1
+    end
   end
 
   film_array
@@ -27,25 +36,28 @@ end
 #  of movies by title. play around with puts out other info about a given film.
 
 def film_api_request(api_array)
-  
+  # Need to store film_hash and then extract the individual attributes into
+  # a new array of hashes
+  film_data_hash = []
   api_array.each do |film_url|
     film_data = RestClient.get(film_url)
-    film_hash = JSON.parse(film_data)
+    film_data_hash  << JSON.parse(film_data)
   end
-
+    film_data_hash
 end
 
-#get_character_movies_from_api("character")
-
-film_api_request(get_character_movies_from_api("character"))
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each.with_index do |movie, i|
+      puts "#{i + 1}. #{movie["title"]}"
+  end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  unformatted_films_hash = get_character_movies_from_api(character)
+  formatted_films_hash = film_api_request(unformatted_films_hash)
+  parse_character_movies(formatted_films_hash)
 end
 
 ## BONUS
