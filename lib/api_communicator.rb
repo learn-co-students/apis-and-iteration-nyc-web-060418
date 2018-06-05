9806c351ebc05d300f3507eaf955947f6b69beb4
github username <github email address>
2018-06-05 16:55:29 -0400

    tidied up with refactoring methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -11,23 +11,37 @@ def get_character_movies_from_api(character)
   all_characters = RestClient.get("https://swapi.co/api/people/?page=" + "#{x}")
   character_hash = JSON.parse(all_characters)
   x +=1
-  character_hash["results"].each do |hash|
-    hash.find do |name, value|
-      if name == "name" && value.downcase == "#{character}"
-        hash["films"].each do |url|
-          collected_films << url
-        end
-      end
-    end
+  find_character_urls(character_hash, character, hash, collected_films)
+  make_film_hash(collected_films, films_hash)
   end
+  films_hash
+end
+
+def make_film_hash(collected_films, films_hash)
   collected_films.each do |url|
     film_api = RestClient.get(url)
     film_info_hash = JSON.parse(film_api)
     films_hash[url] = film_info_hash
   end
 end
-  films_hash
+
+def find_character_urls(character_hash, character, hash, collected_films)
+  character_hash["results"].each do |hash|
+  hash.find do |name, value|
+    if name == "name" && value.downcase == "#{character}"
+      collect_urls(hash, collected_films)
+      break
+    end
+  end
 end
+end
+
+def collect_urls(hash, collected_films)
+  hash["films"].each do |url|
+    collected_films << url
+  end
+end
+
 
 def parse_character_movies(films_hash)
   titles_array = []
