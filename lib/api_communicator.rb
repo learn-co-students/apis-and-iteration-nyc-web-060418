4cd31509d94eab8efef636f2b99275ac1a07b8c5
DevinPierce <devinmpierce@gmail.com>
2018-06-05 17:55:09 -0400

    completed 'get character movies' program, for real this time, with all the pages

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,24 +3,34 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
   films_arr = []
-  character_hash['results'].each do |data|
-    if data['name'].downcase.include?(character)
-      film_urls_array = data['films']
-      film_urls_array.each do |url|
-        film_json = RestClient.get(url)
-        film_hash = JSON.parse(film_json)
-        films_arr << film_hash
+  page = 0
+  hit = false
+  loop do
+    page_url = "http://www.swapi.co/api/people/?page=#{page += 1}"
+    people_hash = JSON.parse(RestClient.get(page_url))
+    character_arr = people_hash['results']
+    character_arr.each do |data|
+      if data['name'].downcase == character.downcase
+        film_urls_array = data['films']
+        film_urls_array.each do |url|
+          film_hash = JSON.parse(RestClient.get(url))
+          films_arr << film_hash
+        end
+        hit = true
       end
+    end
+    if hit == true
+      break
+    elsif people_hash['next'] == nil
+      break
     end
   end
   films_arr
 end
 
 def parse_character_movies(films_arr)
-  films_arr.each {|film| puts film['title']}
+  p films_arr.map {|film| film['title']}
 end
 
 def show_character_movies(character)
