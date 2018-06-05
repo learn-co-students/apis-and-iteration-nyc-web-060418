f4af1c63826d334671c061e0a66a3cb8d270f83d
github username <github email address>
2018-06-05 17:31:02 -0400

    Another commit.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,28 +1,38 @@
+# June 5 2018
+# Star Wars API Lab
+# by Laura Nadolski and Risher Randall
+
 require 'rest-client'
 require 'json'
 require 'pry'
 
 
-def web_request_helper(API_url)
-  film_information = RestClient.get(API_url)
+def web_request_helper(url)
+  film_information = RestClient.get(url)
   film_information_hash = JSON.parse(film_information)
-
-  binding.pry
-
+  return film_information_hash
 end
+
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
+  helper_array = []
+  api_link = 'http://www.swapi.co/api/people/'
 
+  while helper_array.length == 0
+    all_characters = RestClient.get(api_link)
+    character_hash = JSON.parse(all_characters)
 
-  binding.pry
-
-  character_hash["results"].map do |character_array|
-    if character_array["name"] == character
-      character_array["films"].map do |film|
-        film
+    character_hash["results"].each do |character_array|
+      if character_array["name"].downcase == character.downcase
+        character_array["films"].map do |film|
+          helper_array.push web_request_helper(film)
+        end
+      end
+    end
+        api_link = character_hash["next"]
+  end
+  return helper_array
 
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
@@ -37,6 +47,10 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each_with_index do |movie_hash, index|
+    movie_title = movie_hash["title"]
+    puts "#{index + 1} " + movie_title
+  end
 end
 
 def show_character_movies(character)
@@ -45,7 +59,6 @@ def show_character_movies(character)
 end
 
 
-get_character_movies_from_api("luke")
 
 ## BONUS
 
