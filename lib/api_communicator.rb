61dab0d33cf0bc1df6a2a713e8361645f272eea2
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-06-05 14:37:46 -0400

    implemented bonus functions

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,35 +6,40 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+  format_film_hash(film_hash_to_array(character_hash, character))
+end
 
+def format_film_hash(array)
+  films_hash = {}
+  array.each do |url|
+    films_hash[url] = JSON.parse(RestClient.get(url))
+  end
+  films_hash
+end
 
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
+def film_hash_to_array(hash, character)
   films_array = nil
-  character_hash["results"].each do |attribute|
+  hash["results"].each do |attribute|
     if attribute["name"] == character
       films_array = attribute["films"]
     end
   end
+  films_array
+end
 
 
 
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
-  films_hash = {}
-  films_array.each do |url|
-    films_hash[url] = RestClient.get(url)
-    films_hash[url] = JSON.parse(films_hash[url])
-  end
 
   # return value of this method should be collection of info about each film.
-  films_hash
+
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
 
-end
+
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
@@ -65,7 +70,48 @@ end
 
 def search_user_input
   puts "Input search term: "
-  get_user_input
+  user = get_user_input
+  format_entry_hash(return_entry_hash(user))
 end
 
-show_character_movies("Luke Skywalker")
+def return_entry_hash(input_string)
+  all_data = RestClient.get('http://www.swapi.co/api/')
+  data_hash = JSON.parse(all_data)
+  #sample_data = RestClient.get('http://www.swapi.co/api/people/')
+  data_hash.each do |category, url|
+    url_info = JSON.parse(RestClient.get(url))
+
+    url_info["results"].each do |entry|
+      if entry["name"]
+        if entry["name"] == input_string
+          return entry
+        end
+      elsif entry["title"]
+        if entry["title"]== input_string
+          return entry
+        end
+      end
+    end
+  end
+end
+
+def format_entry_hash(hash)
+  hash.each do |category,value|
+    if value.class == String
+      puts "#{category.capitalize}: #{value}."
+    elsif value.class == Array
+      display_string = "#{category.capitalize}: "
+      value.each do |url|
+        url_info = JSON.parse(RestClient.get(url))
+        if url_info["name"]
+          display_string += url_info["name"] + ", "
+        elsif url_info["title"]
+          display_string += url_info["title"] + ", "
+        end
+      end
+      puts display_string.chomp(", ")
+    end
+  end
+end
+
+search_user_input
